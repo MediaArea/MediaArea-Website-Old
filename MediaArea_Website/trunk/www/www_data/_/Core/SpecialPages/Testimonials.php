c2429e4a37f7cbdf039ceeab835ee713b0f95fa1
zenitram <zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2015-02-19 21:16:46 +0000

    GSoC

--- MediaArea_Website/trunk/www/www_data/_/Core/SpecialPages/Testimonials.php
+++ MediaArea_Website/trunk/www/www_data/_/Core/SpecialPages/Testimonials.php
@@ -7,4 +7,8 @@ if (strpos($Page, "/PreForma_MediaInfo_Phase1")!=FALSE) {
     //Page with no ads, always
     $Donated='Y';
 }
+if (strpos($Page, "/GSoC")!=FALSE) {
+    //Page with no ads, always
+    $Donated='Y';
+}
 ?>
