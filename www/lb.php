fb51e641b36534184df994af93aa0b6e250715d2
XhmikosR <xhmikosr@gmail.com>
2015-06-12 19:06:56 +0000

    Minor formatting tweaks.

--- www/lb.php
+++ www/lb.php
@@ -1,10 +1,7 @@
 <?php
-if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-{
-  echo $_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-else
-{
-  echo $_SERVER['SERVER_ADDR'];
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    echo $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    echo $_SERVER['SERVER_ADDR'];
 }
 ?>
--- www/www_data/_/Core/Config.php
+++ www/www_data/_/Core/Config.php
@@ -4,49 +4,47 @@
 header("Cache-Control: no-cache, must-revalidate, max-age=0");
 
 //HTTPS detection
-if (isset($_SERVER['HTTPS']))
-{
-    if ($_SERVER['HTTPS'] == 'on')
-        $IsSecure=1;
+if (isset($_SERVER['HTTPS'])) {
+    if ($_SERVER['HTTPS'] === 'on')
+        $IsSecure = 1;
     else
-        $IsSecure=0;
-}
-else
-    $IsSecure=0;
+        $IsSecure = 0;
+} else
+    $IsSecure = 0;
 
 //User config
 if (isset($_COOKIE["Language"]))
-    $Language=$_COOKIE["Language"];
+    $Language = $_COOKIE["Language"];
 if (isset($_COOKIE["LastVisited"]))
-    $AlreadyVisited='Y';
+    $AlreadyVisited = 'Y';
 if (isset($_COOKIE["Donated"]))
-    $Donated=$_COOKIE["Donated"]; //time()
+    $Donated = $_COOKIE["Donated"]; //time()
 if (isset($_COOKIE["NewVersionRequested"]))
-    $NewVersionRequested='Y';
+    $NewVersionRequested = 'Y';
 
 //Page = "/a/b/c"
 if (!isset($_SERVER["REQUEST_URI"]))
     exit("error");
-$Page=strtok($_SERVER["REQUEST_URI"], "?");
+$Page = strtok($_SERVER["REQUEST_URI"], "?");
 parse_str(strtok("?"), $Argument);
 
 //Language modification
 if (!isset($Language)) {
     if (isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])) {
-        $Language=strtolower(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2));
-        if (($Language=="zh" || $Language=="pt") && strlen($_SERVER["HTTP_ACCEPT_LANGUAGE"])>5 && $_SERVER["HTTP_ACCEPT_LANGUAGE"]{2}=="-") {
-            $Language_Country=strtoupper(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 3, 2));
-            $Language=$Language."-".$Language_Country;
+        $Language = strtolower(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2));
+        if (($Language == "zh" || $Language == "pt") && strlen($_SERVER["HTTP_ACCEPT_LANGUAGE"]) > 5 && $_SERVER["HTTP_ACCEPT_LANGUAGE"]{2} == "-") {
+            $Language_Country = strtoupper(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 3, 2));
+            $Language = $Language . "-" . $Language_Country;
         }
     } else
-        $Language="en";
+        $Language = "en";
 }
 
 //Official website?
-if ($_SERVER["HTTP_HOST"]=="mediaarea.net")
-    $Official="Y";
+if ($_SERVER["HTTP_HOST"] == "mediaarea.net")
+    $Official = "Y";
 
 //Edit?
 if (isset($_REQUEST["Edit"]))
-    $Edit=$_REQUEST["Edit"];
+    $Edit = $_REQUEST["Edit"];
 ?>
